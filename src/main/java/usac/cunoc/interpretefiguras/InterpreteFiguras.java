28a9766b7a5b698f555f92186fa4511fb711ca2f
drymnz <drymnz@archcraft>
2024-08-07 15:00:48 -0600

    El principio del proyecto

---
+++ src/main/java/usac/cunoc/interpretefiguras/InterpreteFiguras.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package usac.cunoc.interpretefiguras;
+
+import usac.cunoc.interpretefiguras.view.views;
+
+/**
+ *
+ * @author drymnz
+ */
+public class InterpreteFiguras {
+
+    public static void main(String[] args) {
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new views().setVisible(true);
+            }
+        });
+    }
+}
